52f01e7a8077ef14dec7ec817c1cc658f9efb782
Dang Khoa <35075796+dangkhoa2016@users.noreply.github.com>
2021-04-16 07:31:50 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { HashRouter as Router, Switch, Route, Redirect, Link } from 'react-router-dom'
 import './base.css'
 import Demo1 from './demos/Demo1'
-import Demo2 from './demos/Demo2'
 
 // Demos inspired by:
 // https://twitter.com/beesandbombs/status/1329796242298245124
@@ -13,16 +12,13 @@ function App() {
     <Router basename="/">
       <div className="frame"></div>
       <div className="content">
-        <h2 className="content__title greeting first">Chúc mừng Khải Lâm Consulting</h2>
-        <h3 className="content__title greeting">Chúc mừng văn phòng mới</h3>
+        <h2 className="content__title greeting">Chúc mừng Khải Lâm Consulting</h2>
+        <h3 className="content__title greeting_last">Chúc mừng văn phòng mới</h3>
       </div>
       <div id="animation">
         <Switch>
           <Route exact path="/demo1">
             <Demo1 />
-          </Route>
-          <Route exact path="/demo2">
-            <Demo2 />
           </Route>
           <Route path="*">
             <Redirect to="/demo1" />
